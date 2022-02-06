786ef476919d41fb339a546e45440b0860a7341b
Artur Kucinski <artuyees@gmail.com>
2022-02-07 00:34:01 +0100

    okok

--- src/components/Molecules/NewImage.jsx
+++ src/components/Molecules/NewImage.jsx
@@ -35,7 +35,7 @@ export default function Account({ session }, props) {
       alert(error.message);
     } finally {
       setLoading(false);
-      window.location.reload(false);
+      window.location.reload();
     }
   }
 
