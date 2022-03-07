ee29038ffad4d09df6d15149777206fb8e1f0878
Artur Kucinski <artuyees@gmail.com>
2022-03-07 18:39:30 +0100

    Added default icon

--- src/components/Atoms/RoundIcon.jsx
+++ src/components/Atoms/RoundIcon.jsx
@@ -29,16 +29,22 @@ function RoundIcon({ data, click }) {
       }}
       className="flex flex-col items-center gap-2"
     >
-      <div className="w-fit rounded-full bg-gradient-to-tr p-[3px] from-[#dfab01]  via-[#ff0080] to-[#820086]">
+      <div className="w-fit rounded-full bg-gradient-to-tr overflow p-[3px] from-[#dfab01]  via-[#ff0080] to-[#820086]">
         <div className="flex flex-col justify-between h-full bg-black rounded-full p-[3px]">
           <div
             className={`w-12 h-12 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full `}
           >
-            <img
-              src={avatarUrl}
-              alt=""
-              className="object-cover h-12 w-12 rounded-full"
-            />
+            {avatarUrl ? (
+              <img
+                src={avatarUrl}
+                alt=""
+                className="object-cover h-12 w-12 rounded-full"
+              />
+            ) : (
+              <div className="w-full rounded-full overflow-hidden h-full justify-center items-center flex">
+                <i className="bx bx-user text-[52px] flex text-center"></i>
+              </div>
+            )}
           </div>
         </div>
       </div>
