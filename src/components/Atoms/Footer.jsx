e147d66c129ff0fdbec45a7042d22a2521563220
Artur Kuciński <artuyees@gmail.com>
2022-09-09 17:56:27 +0200

    Updated footer

--- src/components/Atoms/Footer.jsx
+++ src/components/Atoms/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Footer() {
   return (
     <div className="flex flex-row mt-12 p-1 w-full justify-center text-gray-500">
-      <a href="http://github.com/artuyees" className="w-fit">
-        Artuyees || Artur Kucinski || 2022
+      <a href="http://github.com/akucinskii" className="w-fit">
+         Artur Kucinski || 2022
       </a>
     </div>
   );
